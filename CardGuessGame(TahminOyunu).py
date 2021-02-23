6c4b7e287fa5cee67f295e6ee96fdff65813b176
Mustafa <67900754+msimsec@users.noreply.github.com>
2021-02-24 00:28:12 +0300

    Update CardGuessGame(TahminOyunu).py

--- CardGuessGame(TahminOyunu).py
+++ CardGuessGame(TahminOyunu).py
@@ -1,15 +1,15 @@
 from random import shuffle
 
-def shuffle_list(list):
+def shuffle_list(list): #Liste Karistirılıyor
     shuffle(list)
     return list
-def player_guess():
+def player_guess(): # Uzerinde M harfi bulunan kutu tahmin edilmeye calisiliyor.
     guess=''
     while guess not in ['0','1','2','3','4','5','6','7']:
         print("Pick a number:0,1,2,3,4,5,6,7")
         guess=input()
     return int(guess)
-def check_guess(list,guess):
+def check_guess(list,guess): #Karistirilan liste ve tahmin edilen index karsilastiriliyor ve Sonuc yazdiriliyor.
     if list[guess]=='M':
         print("Correct!")
         print(list)
@@ -19,4 +19,4 @@ def check_guess(list,guess):
 
 list=[' ','M',' ',' ',' ',' ',' ',' ']
 
-check_guess(shuffle_list(list),player_guess())
+check_guess(shuffle_list(list),player_guess()) #Karistirma methodu ve tahmin methodları cagriliyor.
